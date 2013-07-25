1d62931720600b8ff2e7ad2f4434965ef9c8a471
vanilla thunder <marat@marat-bedoev.net>
2013-07-25 23:50:43 +0200

    update to v1.1

--- README.md
+++ README.md
@@ -1,31 +1,24 @@
-# TinyMCE 3.5.8 for OXID eShop CE  4.6+
+# TinyMCE 4.0.1 for OXID eShop CE  4.7+
 TinyMCE is a platform independent web based Javascript HTML WYSIWYG editor control released as Open Source under LGPL.
 More information here: http://www.tinymce.com/
 and here: https://github.com/tinymce
 
-## module version 1.0.2 from 2012-12-20
+## module version 1.1.0 from 2013-07-11
 ### changelog  
-* bugfix: changed text haven't been saved without toggling editor
+* updated to TinyMCE version 4.0.1
+* popup size applied to backend frameset
+
 
 ## INSTALLATION
 ### upload over ftp/ssh:
 copy the content of the "copy_this" folder into the shop root directory  
 **if you are uploading files via ftp, switch to the binary transfer mode**  
-### ssh shell + git client:
-navigate into the modules directory and create a "hdi" directory with a "vendormetadata.php" file inside it (can be empty yet)  
-clone remote git repo and switch to the "module" branch.  
-this commands works for debian and centos:
-<pre>mkdir hdi
-cd hdi/
-wget https://raw.github.com/vanilla-thunder/hdi-tinymce/master/copy_this/modules/hdi/vendormetadata.php
-git clone -b module git://github.com/vanilla-thunder/hdi-tinymce.git
-</pre>
 
 
 ## LICENSE AGREEMENT 
    HDI TinyMCE
-   Copyright (C) 2012  HEINER DIRECT GmbH & Co. KG  
-   info:  info@heiner-direct.com  
+   Copyright (C) 2012-2013  HEINER DIRECT GmbH & Co. KG
+   info:  oxid@heiner-direct.com
   
    This program is free software;  
    you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
--- copy_this/modules/hdi/hdi-tinymce/tinymce.php
+++ copy_this/modules/hdi/hdi-tinymce/tinymce.php
@@ -1,54 +1,69 @@
 <?php
-/**
- * HDI TinyMCE
- * Copyright (C) 2012  HEINER DIRECT GmbH & Co. KG
- *  info:  info@heiner-direct.com
- * 
- * This program is free software;
- * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
- * either version 3 of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>
- */
-class tinyMCE extends tinyMCE_parent {
 
-	protected function _generateTextEditor($iWidth, $iHeight, $oObject, $sField, $sStylesheet = null) {
-		$myConfig = $this->getConfig();
-		$sConfigParam = "bTinyMCE_" . $this->getClassName();
+	/**
+	 * HDI TinyMCE
+	 * Copyright (C) 2012-2013  HEINER DIRECT GmbH & Co. KG
+	 * info:  oxid@heiner-direct.com
+	 *
+	 * This program is free software;
+	 * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
+	 * either version 3 of the License, or (at your option) any later version.
+	 *
+	 * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+	 * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+	 * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>
+	 */
 
-		//check if tinyMCE is active for this calss
-		if ($myConfig->getConfigParam($sConfigParam) === true) {
-			$oViewConf = $this->_aViewData["oViewConf"];
-			$smarty = oxUtilsView::getInstance()->getSmarty();
+	class tinyMCE extends tinyMCE_parent
+	{
 
-			//$sEditor = $smarty->fetch($myConfig->getModulesDir()."hdi/hdi-tinymce/test.tpl");
+		protected function _generateTextEditor($iWidth, $iHeight, $oObject, $sField, $sStylesheet = NULL)
+		{
+			$myConfig = oxRegistry::getConfig();
+			$aClasses = $myConfig->getConfigParam("aTinyMCE_classes");
 
-			if ($oObject) {
-				$sInitialValue = '';
-				if ($oObject->$sField instanceof oxField) {
-					$sInitialValue = $oObject->$sField->getRawValue();
-				} else {
-					$sInitialValue = $oObject->$sField->value;
+			//check if tinyMCE is active for this calss
+			if (in_array($this->getClassName(),$aClasses))
+			{
+				$oViewConf = $this->_aViewData["oViewConf"];
+				$smarty = oxRegistry::get("oxUtilsView")->getSmarty();
+
+				//$sEditor = $smarty->fetch($myConfig->getModulesDir()."hdi/hdi-tinymce/test.tpl");
+
+				if ($oObject)
+				{
+					$sInitialValue = '';
+					if ($oObject->$sField instanceof oxField)
+					{
+						$sInitialValue = $oObject->$sField->getRawValue();
+					} else
+					{
+						$sInitialValue = $oObject->$sField->value;
+					}
+					//$oObject->$sField = new oxField(str_replace('[{$shop->currenthomedir}]', $myConfig->getCurrentShopURL(), $sInitialValue), oxField::T_RAW);
+					$smarty->assign("sField", $sField);
+					$smarty->assign("iWidth", (strpos($iWidth, '%') === false) ? $iWidth . 'px' : $iWidth);
+					//$smarty->assign("iHeight", "80%");
+					$smarty->assign( "iHeight", (strpos($iHeight, '%') === false) ? $iHeight . 'px' : $iHeight);
+					$smarty->assign("sContent", $this->_getEditValue($oObject, $sField));
+
+					//external plugins & controls
+					$smarty->assign("extPlugins", $myConfig->getConfigParam("aTinyMCE_external_plugins"));
+					$smarty->assign("extControls", $myConfig->getConfigParam("sTinyMCE_external_controls"));
+
+
+					//var_dump($myConfig->getModulesDir()."hdi/hdi-tinymce/test.tpl");
+					$smarty->assign("cfg", $myConfig);
+					$smarty->assign("oViewConf", $this->_aViewData["oViewConf"]);
+					$sEditor = $smarty->fetch("tinymce.tpl");
 				}
-				//$oObject->$sField = new oxField(str_replace('[{$shop->currenthomedir}]', $myConfig->getCurrentShopURL(), $sInitialValue), oxField::T_RAW);
-				$smarty->assign( "sField", $sField);
-				$smarty->assign( "iWidth", (strpos($iWidth, '%') === false) ? $iWidth . 'px' : $iWidth);
-				$smarty->assign( "iHeight", "80%");
-				//$smarty->assign( "iHeight", (strpos($iHeight, '%') === false) ? $iHeight . 'px' : $iHeight);
-				$smarty->assign( "sContent", $this->_getEditValue($oObject, $sField));
 
-				//var_dump($myConfig->getModulesDir()."hdi/hdi-tinymce/test.tpl");
-				$smarty->assign( "cfg", $myConfig);
-				$smarty->assign( "oViewConf", $this->_aViewData["oViewConf"]);
-				$sEditor = $smarty->fetch("tinymce.tpl");
+				return $sEditor;
+			} else
+			{
+				//returning default textarea or whatever
+				return parent::_generateTextEditor($iWidth, $iHeight, $oObject, $sField, $sStylesheet);
 			}
-			return $sEditor;
-		} else {
-			//returning default textarea or whatever
-			return parent::_generateTextEditor($iWidth, $iHeight, $oObject, $sField, $sStylesheet);
 		}
-	}
 
-}
+	}
