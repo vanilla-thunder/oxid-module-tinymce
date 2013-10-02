54431935e501aea3eb961cf7f1545193ffffab85
Marat.Bedoev@heiner-direct.com <Marat.Bedoev@heiner-direct.com>
2013-10-02 17:48:32 +0200

    update to 1.1.4

--- copy_this/modules/hdi/hdi-tinymce/metadata.php
+++ copy_this/modules/hdi/hdi-tinymce/metadata.php
@@ -17,10 +17,10 @@
 	$sMetadataVersion = '1.1';
 	$aModule = array(
 		'id'          => 'hdi-tinymce',
-		'title'       => '<strong style="color:#006a8c;border: 1px solid #e30061;padding: 0 2px;background:white;">HDI</strong> <strong>TinyMCE 4.0.5</strong>',
+		'title'       => '<strong style="color:#006a8c;border: 1px solid #e30061;padding: 0 2px;background:white;">HDI</strong> <strong>TinyMCE 4.0.6</strong>',
 		'description' => 'backend implementation of TinyMCE Editor<br/>visit <a href="http://www.tinymce.com/" target="_blank">http://www.tinymce.com/</a> for demo and more details',
 		'thumbnail'   => 'hdi.png',
-		'version'     => '1.1.3 (2013-09-04) / newest version: <img src="https://raw.github.com/vanilla-thunder/hdi-tinymce/master/copy_this/modules/hdi/hdi-tinymce/version.jpg" style="float:right;"/><br/>
+		'version'     => '1.1.4 (2013-10-01) / newest version: <img src="https://raw.github.com/vanilla-thunder/hdi-tinymce/master/copy_this/modules/hdi/hdi-tinymce/version.jpg" style="float:right;"/><br/>
 							<a style="display: inline-block; padding: 1px 25px; background: dodgerblue; border: 1px solid #585858; color: white;" href="https://github.com/vanilla-thunder/hdi-tinymce/" target="_blank">info</a>&nbsp;
 							<a style="display: inline-block; padding: 1px 25px; background: forestgreen; border: 1px solid #585858; color: white;" href="https://github.com/vanilla-thunder/hdi-tinymce/archive/master.zip">download</a>',
 		'author'      => 'Marat Bedoev, HEINER DIRECT GmbH & Co. KG',
@@ -32,7 +32,7 @@
 			'content_main'    => 'hdi/hdi-tinymce/tinymce',
 			'newsletter_main' => 'hdi/hdi-tinymce/tinymce',
 			'news_text'       => 'hdi/hdi-tinymce/tinymce'
-			//'oxAdminDetails'  => 'hdi/hdi-tinymce/tinymce'
+			//'your_class'    => 'hdi/hdi-tinymce/tinymce' // <= insert here your own class
 		),
 		'templates'   => array(
 			'tinymce.tpl' => 'hdi/hdi-tinymce/views/tinymce.tpl',
@@ -43,7 +43,17 @@
 		'settings'    => array(
 			/* enabling tinyMCE for this classes */
 			array('group' => 'tinyMceMain', 'name' => 'aTinyMCE_classes', 'type' => 'arr', 'value' => array("article_main", "category_text","content_main","newsletter_main","news_text"), 'position' => 0),
-            array('group' => 'tinyMceMain', 'name' => 'aTinyMCE_plaincms','type' => 'arr', 'value' => array("oxadminordernpplainemail", "oxadminorderplainemail", "oxemailfooterplain", "oxnewsletterplainemail", "oxordersendplainemail", "oxregisterplainemail", "oxuserorderemailendplain", "oxuserordernpplainemail", "oxuserorderplainemail", "oxupdatepassinfoplainemail", "oxregisterplainaltemail"), 'position' => 1),
+            array('group' => 'tinyMceMain', 'name' => 'aTinyMCE_plaincms','type' => 'arr', 'value' => array(
+                "oxadminorderplainemail","oxadminordernpplainemail", // bestellbenachrichtigung admin + fremdländer
+                "oxuserorderplainemail", "oxuserordernpplainemail", "oxuserorderemailendplain",  // bestellbenachrichtigung user + fremdländer + abschluss
+                "oxordersendplainemail", // versandbestätigung
+                "oxregisterplainemail", "oxregisterplainaltemail", // registrierung
+                "oxupdatepassinfoplainemail", // passwort update
+                "oxnewsletterplainemail", // newsletter
+                "oxemailfooterplain", // email fußtext
+                "oxrighttocancellegend", "oxrighttocancellegend2", // widerrufsrecht
+                "oxstartmetadescription", "oxstartmetakeywords" // META Tags
+                ), 'position' => 1),
 			array('group' => 'tinyMceMain', 'name' => 'sTinyMCE_height',  'type' => 'str', 'value' => '300', 'position' => 2),
 			array('group' => 'tinyMceMain', 'name' => 'sTinyMCE_cssfile', 'type' => 'str', 'value' => '/weiss/ich/noch/nicht.css', 'position' => 3),
 
