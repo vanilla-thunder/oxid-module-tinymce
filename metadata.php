c6c8f68eed64bc057b20f80acc91760361308136
Marat <m@marat.ws>
2015-05-06 23:00:55 +0200

    version 1.4.0

--- metadata.php
+++ metadata.php
@@ -20,9 +20,9 @@
     $aModule          = array(
         'id'          => 'bla-tinymce',
         'title'       => '<strong style="color:#95b900;font-size:125%;">best</strong><strong style="color:#c4ca77;font-size:125%;">life</strong> <strong>TinyMCE</strong>',
-        'description' => 'TinyMCE 4.1.7 WYSIWYG Editor for OXID eShop 4.7 - 4.9<br/>visit <a href="http://www.tinymce.com/" target="_blank">http://www.tinymce.com/</a> for demo and more details<hr/><b style="display: inline-block; float:left;">newest version:</b><img src="' . $v . '" style=" float:left;"/><a style="display: inline-block; padding: 1px 25px; background: dodgerblue;  border: 1px solid #585858; color: white;" href="http://bit.ly/bla-TinyMCE" target="_blank">info</a>&nbsp;<a style="display: inline-block; padding: 1px 25px; background: forestgreen; border: 1px solid #585858; color: white;" href="https://github.com/vanilla-thunder/bla-tinymce/archive/master.zip">download</a>',
+        'description' => 'TinyMCE 4.1.10 WYSIWYG Editor for OXID eShop 4.7 - 4.9<br/>visit <a href="http://www.tinymce.com/" target="_blank">http://www.tinymce.com/</a> for demo and more details<hr/><b style="display: inline-block; float:left;">newest version:</b><img src="' . $v . '" style=" float:left;"/><a style="display: inline-block; padding: 1px 25px; background: dodgerblue;  border: 1px solid #585858; color: white;" href="http://bit.ly/bla-TinyMCE" target="_blank">info</a>&nbsp;<a style="display: inline-block; padding: 1px 25px; background: forestgreen; border: 1px solid #585858; color: white;" href="https://github.com/vanilla-thunder/bla-tinymce/archive/master.zip">download</a>',
         'thumbnail'   => 'tinymce.png',
-        'version'     => '1.4.0 (2015-02-23)',
+        'version'     => '1.4.0 (2015-05-06)',
         'author'      => 'Marat Bedoev, bestlife AG',
         'email'       => 'oxid@bestlife.ag',
         'url'         => 'http://www.bestlife.ag',
