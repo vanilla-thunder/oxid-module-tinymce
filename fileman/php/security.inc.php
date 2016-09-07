cff6e45d50db1a14527937ad30b80212e41e1ab7
Marat <m@marat.ws>
2016-09-07 14:37:52 +0200

    tinymce 4.4.3

--- fileman/php/security.inc.php
+++ fileman/php/security.inc.php
@@ -1,27 +1,25 @@
 <?php
+
 /*
-  RoxyFileman - web based file manager. Ready to use with CKEditor, TinyMCE. 
-  Can be easily integrated with any other WYSIWYG editor or CMS.
+ * ###_COMPANY_### - ###_MODULE_###
+ * Copyright (C) ###_YEAR_###  ###_COMPANY_###
+ * info:  ###_EMAIL_###
+ *
+ * GNU GENERAL PUBLIC LICENSE  
+ *
+ * This program is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>
+ */
 
-  Copyright (C) 2013, RoxyFileman.com - Lyubomir Arsov. All rights reserved.
-  For licensing, see LICENSE.txt or http://RoxyFileman.com/license
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-  Contact: Lyubomir Arsov, liubo (at) web-lobby.com
-*/
-function checkAccess($action){
-  if(!session_id())
-    session_start();
+function checkAccess($action)
+{
+    //if(!session_id()) die("Access Denied!");
+    //if(!session_id()) session_start();
+    //if(isset($_GET['akey'])) $_SESSION['akey'] = strip_tags(preg_replace( "/[^a-zA-Z0-9\._-]/", '', $_GET['akey']));
+	if($_COOKIE['filemanagerkey'] !== md5($_SERVER['HTTP_HOST'].$_COOKIE['admin_sid'])) die('Access Denied!!');
 }
-?>
