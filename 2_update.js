58d2139307f27b846996496f58103da82f7249f1
Marat <m@marat.ws>
2017-06-20 15:35:17 +0200

    tinymce 4.6.4

--- 2_update.js
+++ 2_update.js
@@ -1,3 +1,6 @@
+/*jslint node:true, curly:false */
+"use strict";
+
 var r = require('request'),
     c = require('cheerio'),
     fs = require('fs'),
@@ -15,119 +18,119 @@ var shell = function (command) {
     );
 };
 
-var error = function(err) {
-   console.error("          ############################################################");
-   console.error("          # " + err);
-   console.error("          ############################################################");
-   process.exit();
+var error = function (err) {
+    console.error("          ############################################################");
+    console.error("          # " + err);
+    console.error("          ############################################################");
+    process.exit();
 };
 
 
-var log = function(msg) {
-   var shlomo = "                                                                        |";
-   console.log("   |      " + msg + shlomo.substring(msg.length));
-}
+var log = function (msg) {
+    var shlomo = "                                                                        |";
+    console.log("   |      " + msg + shlomo.substring(msg.length));
+};
 
 
 console.log("");
 console.log("   ___________________________ updating TinyMCE started ___________________________");
 
-r("http://www.tinymce.com/download/", function(err, res, body) {
+r("http://www.tinymce.com/download/", function (err, res, body) {
 
-   if (err || res.statusCode != 200) error(err);
+    if (err || res.statusCode !== 200) error(err);
 
-   if (fs.existsSync("tinymce")) {    
-      shell("rm -rf tinymce");  
-      log("");
-      log("->  removing old tinymce");
-   }
+    if (fs.existsSync("tinymce")) {
+        shell("rm -rf tinymce");
+        log("");
+        log("->  removing old tinymce");
+    }
 
-   var tinymceurl = c.load(body)('section.prod-package a.download-track').first().attr('href');
-   log("");
-   log("->  downloading newest TinyMCE");
-   log("    " + tinymceurl);
+    var tinymceurl = c.load(body)('section.prod-package a.download-track').first().attr('href');
+    log("");
+    log("->  downloading newest TinyMCE");
+    log("    " + tinymceurl);
 
-   r(tinymceurl).pipe(
-      fs.createWriteStream('tmp_tinymce.zip')
-      .on('close', function() {
-         log("");
-         log("->  extracting TinyMCE");
-         var tinymce = new AdmZip('tmp_tinymce.zip');
-         tinymce.getEntries().forEach(function(e) {
-            if (e.entryName.indexOf("tinymce/js/tinymce/") === 0) {
-               tinymce.extractEntryTo(e.entryName, e.entryName.replace("tinymce/js/tinymce", "tinymce").replace(e.name, ""), false, true);
-            }
-         });
-         fs.unlink('tmp_tinymce.zip');
-      })
-   );
+    r(tinymceurl).pipe(
+        fs.createWriteStream('tmp_tinymce.zip')
+            .on('close', function () {
+                log("");
+                log("->  extracting TinyMCE");
+                var tinymce = new AdmZip('tmp_tinymce.zip');
+                tinymce.getEntries().forEach(function (e) {
+                    if (e.entryName.indexOf("tinymce/js/tinymce/") === 0) {
+                        tinymce.extractEntryTo(e.entryName, e.entryName.replace("tinymce/js/tinymce", "tinymce").replace(e.name, ""), false, true);
+                    }
+                });
+                fs.unlink('tmp_tinymce.zip');
+            })
+    );
 
-   log("");
-   log("->  downloading latest language files:");
-   log("    CS, DA, DE, FR, IT, NL, RU");
-   r("https://www.tinymce.com/download/build_language_package?download[]=cs&download[]=da&download[]=de&download[]=fr_FR&download[]=it&download[]=nl&download[]=ru").pipe(
-      fs.createWriteStream('tmp_languages.zip')
-      .on('close', function() {
-         log("");
-         log("->  extracting language files");
-         var languages = new AdmZip('tmp_languages.zip');
-         languages.extractAllTo("tinymce/", true);
-         fs.unlink('tmp_languages.zip');
-      })
-   );
+    log("");
+    log("->  downloading latest language files:");
+    log("    CS, DA, DE, FR, IT, NL, RU");
+    r("https://tinymce-services.azurewebsites.net/1/i18n/download?langs=cs,da,nl,fr_FR,de,it,ru").pipe(
+        fs.createWriteStream('tmp_languages.zip')
+            .on('close', function () {
+                log("");
+                log("->  extracting language files");
+                var languages = new AdmZip('tmp_languages.zip');
+                languages.extractAllTo("tinymce/", true);
+                fs.unlink('tmp_languages.zip');
+            })
+    );
 
 
 });
 
-r("http://www.roxyfileman.com/download", function(err, res, body) {
-   if (err || res.statusCode != 200) error(err);
+r("http://www.roxyfileman.com/download", function (err, res, body) {
+    if (err || res.statusCode !== 200) error(err);
 
-   // (re)moving old tinymce files
-   if (fs.existsSync("fileman")) {
-      shell("rm -rf fileman");
-      log("");
-      log("->  removing old filemanager");
-   }
+    // (re)moving old tinymce files
+    if (fs.existsSync("fileman")) {
+        shell("rm -rf fileman");
+        log("");
+        log("->  removing old filemanager");
+    }
 
-   var roxyurl = 'http://www.roxyfileman.com' + c.load(body)('#content a.btnSpecial').eq(0).attr('href');
-   log("");
-   log("->  downloading newest Roxy Fileman");
-   log("    " + roxyurl);
+    var roxyurl = 'http://www.roxyfileman.com' + c.load(body)('#content a.btnSpecial').eq(0).attr('href');
+    log("");
+    log("->  downloading newest Roxy Fileman");
+    log("    " + roxyurl);
 
-   r(roxyurl).pipe(
-      fs.createWriteStream('tmp_fileman.zip')
-      .on('close', function() {
-         log("");
-         log("->  extracting Roxy Fileman");
-         var zip = new AdmZip('tmp_fileman.zip');
-         zip.extractAllTo("./", true);
-         fs.unlink('tmp_fileman.zip');
-         
-         log("");         
-         log("->  updating Filemanager config");
-         // "FILES_ROOT":"" => "FILES_ROOT": "/out/pictures/wysiwigpro"
-         replace({
-            regex: /\"FILES_ROOT\"\s*\:\s*""\,/,
-            replacement: '"FILES_ROOT": "/out/pictures/wysiwigpro",',
-            paths: ['./fileman/conf.json'],
-            recursive: false,
-            silent: true
-         });
-         // "INTEGRATION":"custom" => "INTEGRATION":"tinymce4",
-         replace({
-            regex: /\"INTEGRATION\"\s*\:\s*"custom"\,/,
-            replacement: '"INTEGRATION": "tinymce4",',
-            paths: ['./fileman/conf.json'],
-            recursive: false,
-            silent: true
-         });
-         shell("cp -f application/core/security.inc.php fileman/php/security.inc.php");
-      })
-   );
+    r(roxyurl).pipe(
+        fs.createWriteStream('tmp_fileman.zip')
+            .on('close', function () {
+                log("");
+                log("->  extracting Roxy Fileman");
+                var zip = new AdmZip('tmp_fileman.zip');
+                zip.extractAllTo("./", true);
+                fs.unlink('tmp_fileman.zip');
+
+                log("");
+                log("->  updating Filemanager config");
+                // "FILES_ROOT":"" => "FILES_ROOT": "/out/pictures/wysiwigpro"
+                replace({
+                    regex: /\"FILES_ROOT\"\s*\:\s*""\,/,
+                    replacement: '"FILES_ROOT": "/out/pictures/wysiwigpro",',
+                    paths: ['./fileman/conf.json'],
+                    recursive: false,
+                    silent: true
+                });
+                // "INTEGRATION":"custom" => "INTEGRATION":"tinymce4",
+                replace({
+                    regex: /\"INTEGRATION\"\s*\:\s*"custom"\,/,
+                    replacement: '"INTEGRATION": "tinymce4",',
+                    paths: ['./fileman/conf.json'],
+                    recursive: false,
+                    silent: true
+                });
+                shell("cp -f application/core/security.inc.php fileman/php/security.inc.php");
+            })
+    );
 });
 
-process.on('exit', function(code) {
-   log("");
-   console.log("   |__________________________ update process finished ___________________________|");
-   console.log("");
+process.on('exit', function (code) {
+    log("");
+    console.log("   |__________________________ update process finished ___________________________|");
+    console.log("");
 });
